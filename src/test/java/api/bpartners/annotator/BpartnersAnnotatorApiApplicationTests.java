c3805254a19090700b5d2fa27748f6c7ab36b159
onitsiky <hei.onitsiky@gmail.com>
2023-10-13 23:33:19 +0300

    infra: initialize spring boot project

---
+++ src/test/java/api/bpartners/annotator/BpartnersAnnotatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.bpartners.annotator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpartnersAnnotatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
