e8704857c07de68b05d3e5d25536bd285288ad9b
onitsiky <hei.onitsiky@gmail.com>
2023-10-19 22:29:10 +0300

    chore: configure custom exceptions

---
+++ src/main/java/api/bpartners/annotator/model/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package api.bpartners.annotator.model.exception;
+
+public class BadRequestException extends ApiException{
+  public BadRequestException(String message) {
+    super(ExceptionType.CLIENT_EXCEPTION, message);
+  }
+}
