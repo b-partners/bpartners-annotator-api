631c90a962f609d18594b6305977cdddc72f899f
Onitsiky <hei.onitsiky@gmail.com>
2023-10-25 10:54:07 +0300

    chore: configure tests
    * docs(api): add endpoint for checking db health
    
    * chore: configure test containers
    
    * chore: add migration for dummy table
    
    * feat: create IT test for security section
    
    * feat: implement dummy endpoint

--- src/main/java/api/bpartners/annotator/endpoint/rest/controller/HealthController.java
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/HealthController.java
@@ -1,13 +1,29 @@
 package api.bpartners.annotator.endpoint.rest.controller;
 
+import api.bpartners.annotator.endpoint.rest.model.DummyComponent;
+import api.bpartners.annotator.service.DummyComponentService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class HealthController {
+  private final DummyComponentService service;
+
+  private DummyComponent toRest(api.bpartners.annotator.repository.jpa.model.DummyComponent domain) {
+    DummyComponent rest = new DummyComponent();
+    rest.setId(domain.getId());
+    return rest;
+  }
 
   @GetMapping("/ping")
   public String checkHealth() {
     return "pong";
+  }
+
+  @GetMapping("/dummy")
+  public DummyComponent checkDbHealth() {
+    return toRest(service.getDummyTestData());
   }
 }
---
+++ src/test/java/api/bpartners/annotator/integration/conf/AbstractContextInitializer.java
@@ -0,0 +1,30 @@
+package api.bpartners.annotator.integration.conf;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.support.TestPropertySourceUtils;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class AbstractContextInitializer
+    implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+  @Override
+  public void initialize(ConfigurableApplicationContext applicationContext) {
+    PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:13.9")
+        .withUsername("sa")
+        .withPassword("sa")
+        .withDatabaseName("it-db");
+
+    postgresContainer.start();
+
+    String flywayTestdataPath = "classpath:/db/testdata";
+    TestPropertySourceUtils.addInlinedPropertiesToEnvironment(
+        applicationContext,
+        "server.port=" + this.getServerPort(),
+        "spring.datasource.url=" + postgresContainer.getJdbcUrl(),
+        "spring.datasource.username=" + postgresContainer.getUsername(),
+        "spring.datasource.password=" + postgresContainer.getPassword(),
+        "spring.flyway.locations=classpath:/db/migration," + flywayTestdataPath);
+  }
+
+  public abstract int getServerPort();
+}
