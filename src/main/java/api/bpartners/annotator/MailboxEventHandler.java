cc0df20faa64209ff84de349a67848a9c28a55c7
Mahefaa <hei.mahefa@gmail.com>
2023-11-02 10:46:51 +0300

    chore: the runner

--- src/main/java/api/bpartners/annotator/MailboxEventHandler.java
+++ src/main/java/api/bpartners/annotator/MailboxEventHandler.java
@@ -1,20 +1,38 @@
 package api.bpartners.annotator;
 
+import api.bpartners.annotator.endpoint.event.EventConf;
+import api.bpartners.annotator.endpoint.event.EventConsumer;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+import static api.bpartners.annotator.endpoint.event.EventConsumer.toAcknowledgeableTypedEvent;
 
 @Slf4j
 public class MailboxEventHandler implements RequestHandler<SQSEvent, String> {
+
   @Override
   public String handleRequest(SQSEvent event, Context context) {
     log.info("Following received events : %s\n", event);
     List<SQSEvent.SQSMessage> messages = event.getRecords();
     log.info("Following received messages : %s\n", messages);
 
-    //eventConsumer.accept(toAcknowledgeableTypedEvent(eventConf, sqsClient, messages));
+    ConfigurableApplicationContext applicationContext = applicationContext();
+    EventConsumer eventConsumer = applicationContext.getBean(EventConsumer.class);
+    EventConf eventConf = applicationContext.getBean(EventConf.class);
+    SqsClient sqsClient = applicationContext.getBean(SqsClient.class);
+
+    eventConsumer.accept(toAcknowledgeableTypedEvent(eventConf, sqsClient, messages));
+
     return "{message: ok}";
+  }
+
+  private ConfigurableApplicationContext applicationContext(String... args) {
+    return SpringApplication.run(BpartnersAnnotatorApiApplication.class, args);
   }
 }
