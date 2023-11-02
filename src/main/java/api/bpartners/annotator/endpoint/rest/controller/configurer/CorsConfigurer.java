a083697c15fc551dfd1e9e16cd4d75f0a2187223
Mahefaa <hei.mahefa@gmail.com>
2023-11-02 13:29:46 +0300

    chore: cors config allows all methods and headers on all paths

---
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/configurer/CorsConfigurer.java
@@ -0,0 +1,15 @@
+package api.bpartners.annotator.endpoint.rest.controller.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer implements WebMvcConfigurer {
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedMethods("*")
+        .allowedHeaders("*");
+  }
+}
