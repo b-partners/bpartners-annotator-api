e6f08e8d6ed5fe3f3efa311567911009fb1d41c2
Onitsiky <hei.onitsiky@gmail.com>
2023-11-03 11:51:43 +0300

    feat: add ses sendEmail

--- src/main/java/api/bpartners/annotator/endpoint/event/EventConf.java
+++ src/main/java/api/bpartners/annotator/endpoint/event/EventConf.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
 
 @Configuration
@@ -12,16 +13,27 @@ public class EventConf {
   private final Region region;
   @Getter
   private final String sqsQueue;
+  @Getter
+  private final String sesSource;
 
   public EventConf(@Value("${aws.region}") Region region,
-                   @Value("${aws.sqs.queue.url}") String sqsQueue) {
+                   @Value("${aws.sqs.queue.url}") String sqsQueue,
+                   @Value("${aws.ses.source}") String sesSource) {
     this.region = region;
     this.sqsQueue = sqsQueue;
+    this.sesSource = sesSource;
   }
 
   @Bean
   public SqsClient getSqsClient() {
     return SqsClient.builder()
+        .region(region)
+        .build();
+  }
+
+  @Bean
+  public SesClient getSesClient() {
+    return SesClient.builder()
         .region(region)
         .build();
   }
