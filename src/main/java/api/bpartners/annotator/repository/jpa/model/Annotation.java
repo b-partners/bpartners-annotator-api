0a8b6a2268f9517fd1497f8f8c2cdc42613da2c5
Mahefaa <hei.mahefa@gmail.com>
2023-11-03 15:45:30 +0300

    fix: Annotation ID is not a generated value, set task job as the saved one before updating it

--- src/main/java/api/bpartners/annotator/repository/jpa/model/Annotation.java
+++ src/main/java/api/bpartners/annotator/repository/jpa/model/Annotation.java
@@ -1,13 +1,16 @@
 package api.bpartners.annotator.repository.jpa.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
-
-import java.util.List;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Data
@@ -15,27 +18,26 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Annotation {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private String id;
-    private String taskId;
-    @OneToOne
-    @JoinColumn(name = "label_id", updatable = false)
-    private Label label;
-    private String userId;
-    @JdbcTypeCode(SqlTypes.JSON)
-    private Polygon polygon;
+  @Id
+  private String id;
+  private String taskId;
+  @OneToOne
+  @JoinColumn(name = "label_id", updatable = false)
+  private Label label;
+  private String userId;
+  @JdbcTypeCode(SqlTypes.JSON)
+  private Polygon polygon;
 
-    @Data
-    @Builder
-    public static class Polygon {
-        private List<Point> points;
-    }
+  @Data
+  @Builder
+  public static class Polygon {
+    private List<Point> points;
+  }
 
-    @Data
-    @Builder
-    public static class Point {
-        private double x;
-        private double y;
-    }
+  @Data
+  @Builder
+  public static class Point {
+    private double x;
+    private double y;
+  }
 }
