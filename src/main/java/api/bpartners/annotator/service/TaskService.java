213fd796aeca076147a2364ed6402375cc79a016
Onitsiky <hei.onitsiky@gmail.com>
2023-11-03 16:00:02 +0300

    feat: paginate get all tasks
    * docs(api): add pagination for getting all tasks
    
    * chore: add pagination models
    
    * feat: paginate tasks

--- src/main/java/api/bpartners/annotator/service/TaskService.java
+++ src/main/java/api/bpartners/annotator/service/TaskService.java
@@ -1,10 +1,14 @@
 package api.bpartners.annotator.service;
 
+import api.bpartners.annotator.model.BoundedPageSize;
+import api.bpartners.annotator.model.PageFromOne;
 import api.bpartners.annotator.model.exception.NotFoundException;
 import api.bpartners.annotator.repository.jpa.TaskRepository;
 import api.bpartners.annotator.repository.jpa.model.Task;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import static api.bpartners.annotator.repository.jpa.model.enums.TaskStatus.PENDING;
@@ -15,8 +19,11 @@ import static api.bpartners.annotator.repository.jpa.model.enums.TaskStatus.UNDE
 public class TaskService {
   private final TaskRepository repository;
 
-  public List<Task> getAllByJob(String jobId) {
-    return repository.findAllByJobId(jobId);
+  public List<Task> getAllByJob(String jobId, PageFromOne page, BoundedPageSize pageSize) {
+    int pageValue = page != null ? page.getValue() - 1 : 0;
+    int pageSizeValue = pageSize != null ? pageSize.getValue() : 30;
+    Pageable pageable = PageRequest.of(pageValue, pageSizeValue);
+    return repository.findAllByJobId(jobId, pageable);
   }
 
   public Task getByJobIdAndId(String jobId, String id) {
