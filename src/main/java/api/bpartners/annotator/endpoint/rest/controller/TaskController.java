2a25b581e5c4d499eee9b820b5fa9ce3359af224
Onitsiky <hei.onitsiky@gmail.com>
2023-11-03 15:55:23 +0300

    feat: paginate tasks

--- src/main/java/api/bpartners/annotator/endpoint/rest/controller/TaskController.java
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/TaskController.java
@@ -2,11 +2,14 @@ package api.bpartners.annotator.endpoint.rest.controller;
 
 import api.bpartners.annotator.endpoint.rest.controller.mapper.TaskMapper;
 import api.bpartners.annotator.endpoint.rest.model.Task;
+import api.bpartners.annotator.model.BoundedPageSize;
+import api.bpartners.annotator.model.PageFromOne;
 import api.bpartners.annotator.service.TaskService;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
@@ -16,8 +19,10 @@ public class TaskController {
   private final TaskMapper mapper;
 
   @GetMapping("/jobs/{jobId}/tasks")
-  public List<Task> getTasks(@PathVariable String jobId) {
-    return service.getAllByJob(jobId).stream().map(mapper::toRest).toList();
+  public List<Task> getTasks(@PathVariable String jobId,
+                             @RequestParam(required = false) PageFromOne page,
+                             @RequestParam(required = false) BoundedPageSize pageSize) {
+    return service.getAllByJob(jobId, page, pageSize).stream().map(mapper::toRest).toList();
   }
 
   @GetMapping("/jobs/{jobId}/tasks/{taskId}")
--- src/main/java/api/bpartners/annotator/repository/jpa/TaskRepository.java
+++ src/main/java/api/bpartners/annotator/repository/jpa/TaskRepository.java
@@ -4,10 +4,11 @@ import api.bpartners.annotator.repository.jpa.model.Task;
 import api.bpartners.annotator.repository.jpa.model.enums.TaskStatus;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, String> {
-  List<Task> findAllByJobId(String jobId);
+  List<Task> findAllByJobId(String jobId, Pageable pageable);
 
   Optional<Task> findByJobIdAndId(String jobId, String id);
   Task findFirstByJobIdAndStatus(String jobId, TaskStatus status);
