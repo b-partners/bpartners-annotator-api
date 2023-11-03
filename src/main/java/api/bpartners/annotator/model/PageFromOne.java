ddcdf0c41b643f69493e90a3025b2884e0b72585
Onitsiky <hei.onitsiky@gmail.com>
2023-11-03 15:50:25 +0300

    chore: add pagination models

---
+++ src/main/java/api/bpartners/annotator/model/PageFromOne.java
@@ -0,0 +1,17 @@
+package api.bpartners.annotator.model;
+
+import api.bpartners.annotator.model.exception.BadRequestException;
+import lombok.Getter;
+
+public class PageFromOne {
+  public static final int MIN_PAGE = 1;
+  @Getter
+  private final int value;
+
+  public PageFromOne(int value) {
+    if (value < MIN_PAGE) {
+      throw new BadRequestException("page must be >=1");
+    }
+    this.value = value;
+  }
+}
