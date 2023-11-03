5dac6ef5218e9444423752fdef839bbaa36bea8f
Onitsiky <hei.onitsiky@gmail.com>
2023-11-03 13:17:02 +0300

    feat: configure ses send email
    * chore: add ses dependency
    
    * feat: add ses sendEmail
    
    * fix: tests env vars

---
+++ src/main/java/api/bpartners/annotator/service/aws/SesService.java
@@ -0,0 +1,48 @@
+package api.bpartners.annotator.service.aws;
+
+import api.bpartners.annotator.endpoint.event.EventConf;
+import api.bpartners.annotator.repository.jpa.model.exception.ApiException;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.Body;
+import software.amazon.awssdk.services.ses.model.Content;
+import software.amazon.awssdk.services.ses.model.Message;
+import software.amazon.awssdk.services.ses.model.SendEmailRequest;
+
+import static api.bpartners.annotator.repository.jpa.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
+
+@Service
+@AllArgsConstructor
+public class SesService {
+  private final SesClient client;
+  private final EventConf eventConf;
+
+  public void sendEmail(String recipient, String subject, String htmlBody) {
+    Message message = Message.builder()
+        .subject(Content.builder()
+            .data(subject)
+            .build())
+        .body(Body.builder()
+            .html(Content.builder()
+                .data(htmlBody)
+                .build())
+            .build())
+        .build();
+    SendEmailRequest emailRequest = SendEmailRequest.builder()
+        .source(eventConf.getSesSource())
+        .destination(destination -> destination.
+            toAddresses(recipient))
+        .message(message)
+        .build();
+
+    try {
+      client.sendEmail(emailRequest);
+    } catch (AwsServiceException | SdkClientException exception) {
+      throw new ApiException(SERVER_EXCEPTION, exception.getMessage());
+    }
+
+  }
+}
