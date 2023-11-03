82f26bd3de8acf0ce535bca52f3087b6291a18e5
Mahefaa <hei.mahefa@gmail.com>
2023-11-03 10:00:10 +0300

    fix: label ID is not a generated value, drop unique constraint on label name

--- src/main/java/api/bpartners/annotator/repository/jpa/model/Label.java
+++ src/main/java/api/bpartners/annotator/repository/jpa/model/Label.java
@@ -1,14 +1,11 @@
 package api.bpartners.annotator.repository.jpa.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Data
@@ -17,7 +14,6 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 public class Label {
   @Id
-  @GeneratedValue(strategy = IDENTITY)
   private String id;
   private String name;
 }
