64f94871eac0ebca84a7a2c7fed6c76e3d9e596b
Onitsiky <hei.onitsiky@gmail.com>
2023-11-09 08:58:44 +0300

    fix: packages for event

--- src/test/java/api/bpartners/annotator/endpoint/event/EventConsumerTest.java
+++ src/test/java/api/bpartners/annotator/endpoint/event/EventConsumerTest.java
@@ -28,7 +28,7 @@ class EventConsumerTest extends FacadeTest {
         List.of(
             new EventConsumer.AcknowledgeableTypedEvent(
                 new EventConsumer.TypedEvent(
-                    "com.company.base.endpoint.event.gen.UuidCreated", payloadReceived),
+                    "api.bpartners.annotator.endpoint.event.gen.UuidCreated", payloadReceived),
                 () -> {})));
 
     Thread.sleep(2_000);
