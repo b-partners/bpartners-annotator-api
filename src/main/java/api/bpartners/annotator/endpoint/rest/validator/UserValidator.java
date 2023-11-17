596f95ae6388e1dd146fd5d4b75c6f0d1fc37b11
Mahefaa <hei.mahefa@gmail.com>
2023-11-17 12:17:33 +0300

    feat: users

---
+++ src/main/java/api/bpartners/annotator/endpoint/rest/validator/UserValidator.java
@@ -0,0 +1,31 @@
+package api.bpartners.annotator.endpoint.rest.validator;
+
+import api.bpartners.annotator.endpoint.rest.model.User;
+import api.bpartners.annotator.endpoint.rest.model.UserRole;
+import api.bpartners.annotator.model.exception.BadRequestException;
+import java.util.function.Consumer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserValidator implements Consumer<User> {
+  @Override
+  public void accept(User user) {
+    StringBuilder exceptionMessageBuilder = new StringBuilder();
+    if (user.getEmail() == null) {
+      exceptionMessageBuilder.append("email is mandatory");
+    }
+    if (user.getEmail().isBlank()) {
+      exceptionMessageBuilder.append("email must not be blank. ");
+    }
+    if (user.getRole() == null) {
+      exceptionMessageBuilder.append("role is mandatory. ");
+    }
+    if (user.getRole() != UserRole.ANNOTATOR) {
+      exceptionMessageBuilder.append("only annotator role is supported. ");
+    }
+    String exceptionMessage = exceptionMessageBuilder.toString();
+    if (!exceptionMessage.isBlank()) {
+      throw new BadRequestException(exceptionMessage);
+    }
+  }
+}
