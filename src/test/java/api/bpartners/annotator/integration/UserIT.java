ee90ed9afaa8d2b1d4443bc4fdcd19bcf43e0d11
Onitsiky <hei.onitsiky@gmail.com>
2023-11-20 11:15:41 +0300

    chore(tests): add UserIT

---
+++ src/test/java/api/bpartners/annotator/integration/UserIT.java
@@ -0,0 +1,71 @@
+package api.bpartners.annotator.integration;
+
+import api.bpartners.annotator.conf.FacadeIT;
+import api.bpartners.annotator.endpoint.rest.api.SecurityApi;
+import api.bpartners.annotator.endpoint.rest.client.ApiClient;
+import api.bpartners.annotator.endpoint.rest.client.ApiException;
+import api.bpartners.annotator.endpoint.rest.model.Team;
+import api.bpartners.annotator.endpoint.rest.model.User;
+import api.bpartners.annotator.endpoint.rest.model.UserRole;
+import api.bpartners.annotator.endpoint.rest.security.cognito.CognitoComponent;
+import api.bpartners.annotator.integration.conf.utils.TestUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static api.bpartners.annotator.integration.conf.utils.TestUtils.assertThrowsForbiddenException;
+import static api.bpartners.annotator.integration.conf.utils.TestUtils.setUpCognito;
+import static org.junit.Assert.assertEquals;
+
+@Testcontainers
+@AutoConfigureMockMvc
+public class UserIT extends FacadeIT {
+  @MockBean
+  private CognitoComponent cognitoComponent;
+
+  @LocalServerPort
+  private int port;
+
+  private ApiClient anApiClient() {
+    return TestUtils.anApiClient(TestUtils.JOE_DOE_TOKEN, port);
+  }
+
+  static User restJoeDoeUser() {
+    return new User()
+        .id("joe_doe_id")
+        .email("joe@email.com")
+        .team(joeTeam())
+        .role(UserRole.ANNOTATOR);
+  }
+
+  static Team joeTeam() {
+    return new Team()
+        .id("team_1_id")
+        .name("joe_team");
+  }
+  @BeforeEach
+  public void setUp() {
+    setUpCognito(cognitoComponent);
+  }
+
+  @Test
+  void user_read_own_information_ok() throws ApiException {
+    ApiClient joeDoeClient = anApiClient();
+    SecurityApi api = new SecurityApi(joeDoeClient);
+
+    User actualUser = api.whoami().getUser();
+
+    assertEquals(restJoeDoeUser(), actualUser);
+  }
+
+  @Test
+  void user_read_own_information_ko() {
+    ApiClient unknownClient = new ApiClient();
+    SecurityApi api = new SecurityApi(unknownClient);
+
+    assertThrowsForbiddenException(() -> api.whoami());
+  }
+}
--- src/test/java/api/bpartners/annotator/integration/conf/utils/TestUtils.java
+++ src/test/java/api/bpartners/annotator/integration/conf/utils/TestUtils.java
@@ -1,11 +1,15 @@
 package api.bpartners.annotator.integration.conf.utils;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 import api.bpartners.annotator.endpoint.rest.client.ApiClient;
+import api.bpartners.annotator.endpoint.rest.client.ApiException;
 import api.bpartners.annotator.endpoint.rest.security.cognito.CognitoComponent;
 import java.io.IOException;
 import java.net.ServerSocket;
+import org.junit.jupiter.api.function.Executable;
 
 public class TestUtils {
   public static final String JOE_DOE_TOKEN = "joe_doe_token";
@@ -31,5 +35,13 @@ public class TestUtils {
 
   public static void setUpCognito(CognitoComponent cognitoComponent) {
     when(cognitoComponent.getEmailByToken(JOE_DOE_TOKEN)).thenReturn(JOE_DOE_EMAIL);
+  }
+
+  public static void assertThrowsForbiddenException(Executable executable) {
+    ApiException apiException = assertThrows(ApiException.class, executable);
+    String responseBody = apiException.getResponseBody();
+    assertEquals("{"
+        + "\"type\":\"403 FORBIDDEN\","
+        + "\"message\":\"Bad credentials\"}", responseBody);
   }
 }
