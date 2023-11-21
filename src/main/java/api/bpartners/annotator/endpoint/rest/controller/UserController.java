52519ecd751a0367e7b89861934314b06d74e850
Mahefaa <hei.mahefa@gmail.com>
2023-11-21 15:33:49 +0300

    chore: do not use import *

--- src/main/java/api/bpartners/annotator/endpoint/rest/controller/UserController.java
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/UserController.java
@@ -8,7 +8,11 @@ import api.bpartners.annotator.model.PageFromOne;
 import api.bpartners.annotator.service.UserService;
 import java.util.List;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
