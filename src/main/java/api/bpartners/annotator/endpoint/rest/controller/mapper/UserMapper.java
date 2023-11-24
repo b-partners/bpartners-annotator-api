1719f315b1bf7345c0fb3e16867849dcd0bc493c
Onitsiky <hei.onitsiky@gmail.com>
2023-11-24 11:08:09 +0300

    feat: implement update user team endpoint

--- src/main/java/api/bpartners/annotator/endpoint/rest/controller/mapper/UserMapper.java
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package api.bpartners.annotator.endpoint.rest.controller.mapper;
 import static java.util.UUID.randomUUID;
 
 import api.bpartners.annotator.endpoint.rest.model.CreateUser;
+import api.bpartners.annotator.endpoint.rest.model.TeamUser;
 import api.bpartners.annotator.endpoint.rest.model.User;
 import api.bpartners.annotator.endpoint.rest.security.model.Role;
+import api.bpartners.annotator.endpoint.rest.validator.TeamUserValidator;
 import api.bpartners.annotator.endpoint.rest.validator.UserValidator;
+import api.bpartners.annotator.repository.model.Team;
 import api.bpartners.annotator.service.TeamService;
+import api.bpartners.annotator.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,8 +19,10 @@ import org.springframework.stereotype.Component;
 public class UserMapper {
   private final UserRoleMapper roleMapper;
   private final UserValidator validator;
+  private final TeamUserValidator teamUserValidator;
   private final TeamMapper teamMapper;
   private final TeamService teamService;
+  private final UserService userService;
 
   public User toRest(api.bpartners.annotator.repository.model.User domain) {
     return new User()
@@ -34,5 +40,13 @@ public class UserMapper {
         .roles(new Role[] {roleMapper.toDomain(rest.getRole())})
         .team(teamService.getById(rest.getTeamId()))
         .build();
+  }
+
+  public api.bpartners.annotator.repository.model.User toDomain(TeamUser rest) {
+    teamUserValidator.accept(rest);
+    api.bpartners.annotator.repository.model.User actualUser = userService.getById(rest.getUserId());
+    Team actualTeam = teamService.getById(rest.getTeamId());
+    actualUser.setTeam(actualTeam);
+    return actualUser;
   }
 }
---
+++ src/main/java/api/bpartners/annotator/service/event/UserTeamUpdatedService.java
@@ -0,0 +1,17 @@
+package api.bpartners.annotator.service.event;
+
+import api.bpartners.annotator.endpoint.event.gen.UserTeamUpdated;
+import api.bpartners.annotator.endpoint.rest.security.cognito.CognitoComponent;
+import java.util.function.Consumer;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class UserTeamUpdatedService implements Consumer<UserTeamUpdated> {
+  private final CognitoComponent cognitoComponent;
+  @Override
+  public void accept(UserTeamUpdated userTeamUpdated) {
+    cognitoComponent.addUserToGroup(userTeamUpdated.getGroup(), userTeamUpdated.getUsername());
+  }
+}
