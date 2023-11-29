1ffd5917613e4c33ecd05b62a369e32ecece021c
Mahefaa <hei.mahefa@gmail.com>
2023-11-29 13:22:47 +0300

    feat: get annotations and annotation

--- src/main/java/api/bpartners/annotator/endpoint/rest/controller/AnnotationController.java
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/AnnotationController.java
@@ -1,13 +1,17 @@
 package api.bpartners.annotator.endpoint.rest.controller;
 
+import static java.util.stream.Collectors.toList;
+
 import api.bpartners.annotator.endpoint.rest.controller.mapper.AnnotationMapper;
 import api.bpartners.annotator.endpoint.rest.model.Annotation;
+import api.bpartners.annotator.model.BoundedPageSize;
+import api.bpartners.annotator.model.PageFromOne;
 import api.bpartners.annotator.service.AnnotationService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,12 +20,20 @@ public class AnnotationController {
   private final AnnotationService service;
   private final AnnotationMapper mapper;
 
-  @PutMapping("/users/{userId}/tasks/{taskId}/annotations")
-  public List<Annotation> annotateTask(
-      @PathVariable String userId,
+  @GetMapping("/jobs/{jobId}/tasks/{taskId}/annotations")
+  public List<Annotation> getAnnotationsByJobTask(
+      @PathVariable String jobId,
       @PathVariable String taskId,
-      @RequestBody List<Annotation> annotations) {
-    var saved = service.save(annotations.stream().map(mapper::toDomain).toList());
-    return saved.stream().map(mapper::toRest).toList();
+      @RequestParam PageFromOne page,
+      @RequestParam BoundedPageSize pageSize) {
+    return service.findAllByTask(taskId, page, pageSize).stream()
+        .map(mapper::toRest)
+        .collect(toList());
+  }
+
+  @GetMapping("/jobs/{jobId}/tasks/{taskId}/annotations/{annotationId}")
+  public Annotation getAnnotationByJobTaskAndId(
+      @PathVariable String jobId, @PathVariable String taskId, @PathVariable String annotationId) {
+    return mapper.toRest(service.findByTaskIdAndId(taskId, annotationId));
   }
 }
--- src/main/java/api/bpartners/annotator/service/AnnotationService.java
+++ src/main/java/api/bpartners/annotator/service/AnnotationService.java
@@ -1,9 +1,14 @@
 package api.bpartners.annotator.service;
 
+import api.bpartners.annotator.model.BoundedPageSize;
+import api.bpartners.annotator.model.PageFromOne;
+import api.bpartners.annotator.model.exception.NotFoundException;
 import api.bpartners.annotator.repository.jpa.AnnotationRepository;
 import api.bpartners.annotator.repository.model.Annotation;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,5 +18,23 @@ public class AnnotationService {
 
   public List<Annotation> save(List<Annotation> annotations) {
     return repository.saveAll(annotations);
+  }
+
+  public List<Annotation> findAllByTask(String taskId, PageFromOne page, BoundedPageSize pageSize) {
+    Pageable pageable = PageRequest.of(page.getValue() - 1, pageSize.getValue());
+    return repository.findAllByTaskId(taskId, pageable);
+  }
+
+  public Annotation findByTaskIdAndId(String taskId, String id) {
+    return repository
+        .findByTaskIdAndId(taskId, id)
+        .orElseThrow(
+            () ->
+                new NotFoundException(
+                    "Annotation identified by id = "
+                        + id
+                        + " and taskId = "
+                        + taskId
+                        + " not found"));
   }
 }
