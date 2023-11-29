77af5e41b56e4ef0fb0ef16a190b6293e9695cc8
Mahefaa <hei.mahefa@gmail.com>
2023-11-29 22:40:59 +0300

    feat: admin annotation batch review

---
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/AnnotationBatchReviewController.java
@@ -0,0 +1,53 @@
+package api.bpartners.annotator.endpoint.rest.controller;
+
+import api.bpartners.annotator.endpoint.rest.controller.mapper.AnnotationBatchReviewMapper;
+import api.bpartners.annotator.endpoint.rest.model.AnnotationBatchReview;
+import api.bpartners.annotator.service.AnnotationBatchReviewService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class AnnotationBatchReviewController {
+  private final AnnotationBatchReviewService service;
+  private final AnnotationBatchReviewMapper mapper;
+
+  @GetMapping("/jobs/{jobId}/tasks/{taskId}/annotations/{annotationBatchId}/reviews")
+  public List<AnnotationBatchReview> getJobTaskAnnotationReviews(
+      @PathVariable String jobId,
+      @PathVariable String taskId,
+      @PathVariable String annotationBatchId) {
+    return service.findAllByJobTaskAndAnnotation(jobId, taskId, annotationBatchId).stream()
+        .map(mapper::toRest)
+        .toList();
+  }
+
+  @GetMapping("/jobs/{jobId}/tasks/{taskId}/annotations/{annotationBatchId}/reviews/{reviewId}")
+  public AnnotationBatchReview getJobTaskAnnotationReview(
+      @PathVariable String jobId,
+      @PathVariable String taskId,
+      @PathVariable String annotationBatchId,
+      @PathVariable String reviewId) {
+    return mapper.toRest(
+        service.findByJobTaskAndAnnotationAndId(jobId, taskId, annotationBatchId, reviewId));
+  }
+
+  @PutMapping("/jobs/{jobId}/tasks/{taskId}/annotations/{annotationBatchId}/reviews/")
+  public List<AnnotationBatchReview> crupdateAnnotationReviews(
+      @PathVariable String jobId,
+      @PathVariable String taskId,
+      @PathVariable String annotationBatchId,
+      @RequestBody List<AnnotationBatchReview> annotationBatchReviews) {
+    List<api.bpartners.annotator.repository.model.AnnotationBatchReview> saved =
+        service.saveAll(
+            taskId,
+            annotationBatchId,
+            annotationBatchReviews.stream().map(mapper::toDomain).toList());
+    return saved.stream().map(mapper::toRest).toList();
+  }
+}
---
+++ src/main/java/api/bpartners/annotator/repository/model/enums/ReviewStatus.java
@@ -0,0 +1,6 @@
+package api.bpartners.annotator.repository.model.enums;
+
+public enum ReviewStatus {
+  ACCEPTED,
+  REJECTED;
+}
