aed683b056fd66d3727ba94e8b056178e216bb4b
Mahefaa <hei.mahefa@gmail.com>
2023-11-30 11:07:44 +0300

    chore: check status transition on tasks

--- src/main/java/api/bpartners/annotator/repository/jpa/TaskRepository.java
+++ src/main/java/api/bpartners/annotator/repository/jpa/TaskRepository.java
@@ -2,6 +2,7 @@ package api.bpartners.annotator.repository.jpa;
 
 import api.bpartners.annotator.repository.model.Task;
 import api.bpartners.annotator.repository.model.enums.TaskStatus;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +11,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TaskRepository extends JpaRepository<Task, String> {
   List<Task> findAllByJobId(String jobId, Pageable pageable);
 
+  List<Task> findAllByJobId(String jobId);
+
   Optional<Task> findByJobIdAndId(String jobId, String id);
 
-  Task findFirstByJobIdAndStatus(String jobId, TaskStatus status);
+  Optional<Task> findFirstByJobIdAndStatus(String jobId, TaskStatus status);
+
+  boolean existsByJobIdAndStatusNotIn(String jobId, Collection<TaskStatus> taskStatuses);
 }
--- src/main/java/api/bpartners/annotator/service/TaskService.java
+++ src/main/java/api/bpartners/annotator/service/TaskService.java
@@ -1,14 +1,20 @@
 package api.bpartners.annotator.service;
 
+import static api.bpartners.annotator.repository.model.enums.JobStatus.TO_REVIEW;
+import static api.bpartners.annotator.repository.model.enums.TaskStatus.COMPLETED;
 import static api.bpartners.annotator.repository.model.enums.TaskStatus.PENDING;
+import static api.bpartners.annotator.repository.model.enums.TaskStatus.TO_CORRECT;
 import static api.bpartners.annotator.repository.model.enums.TaskStatus.UNDER_COMPLETION;
 
 import api.bpartners.annotator.model.BoundedPageSize;
 import api.bpartners.annotator.model.PageFromOne;
+import api.bpartners.annotator.model.exception.BadRequestException;
 import api.bpartners.annotator.model.exception.NotFoundException;
 import api.bpartners.annotator.repository.jpa.TaskRepository;
 import api.bpartners.annotator.repository.model.Task;
+import api.bpartners.annotator.repository.model.enums.TaskStatus;
 import java.util.List;
+import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +24,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class TaskService {
   private final TaskRepository repository;
+  private final JobService jobService;
 
   public List<Task> getAllByJob(String jobId, PageFromOne page, BoundedPageSize pageSize) {
     Pageable pageable = PageRequest.of(page.getValue() - 1, pageSize.getValue());
@@ -35,21 +42,57 @@ public class TaskService {
 
   public Task update(String jobId, String id, Task task) {
     Task entity = getByJobIdAndId(jobId, id);
+    checkTaskStatusTransition(entity, task);
     task.setFilename(entity.getFilename());
     task.setJob(entity.getJob());
-    return repository.save(task);
+    Task saved = repository.save(task);
+    if (saved.getStatus() == COMPLETED) {
+      boolean hasJobCompletedAllTasks =
+          repository.existsByJobIdAndStatusNotIn(
+              task.getJob().getId(), List.of(PENDING, UNDER_COMPLETION, TO_CORRECT));
+      if (hasJobCompletedAllTasks) {
+        jobService.updateJobStatus(task.getJob().getId(), TO_REVIEW);
+      }
+    }
+    return saved;
   }
 
   public Task getAvailableTaskFromJob(String teamId, String jobId) {
-    Task availableTask = repository.findFirstByJobIdAndStatus(jobId, PENDING);
-    if (availableTask == null) {
+    Optional<Task> optionalTask = repository.findFirstByJobIdAndStatus(jobId, PENDING);
+    if (optionalTask.isEmpty()) {
       return null;
     }
+    Task availableTask = optionalTask.get();
     availableTask.setStatus(UNDER_COMPLETION);
-    return repository.save(availableTask);
+    return update(jobId, availableTask.getId(), availableTask);
   }
 
   public List<Task> createTasks(List<Task> tasks) {
     return repository.saveAll(tasks);
+  }
+
+  public TaskStatus checkTaskStatusTransition(Task currentTask, Task newTask) {
+    TaskStatus current = currentTask.getStatus();
+    TaskStatus next = newTask.getStatus();
+    BadRequestException exception =
+        new BadRequestException(String.format("illegal transition: %s -> %s", current, next));
+    return switch (current) {
+      case PENDING -> switch (next) {
+        case PENDING, UNDER_COMPLETION -> next;
+        case TO_CORRECT, COMPLETED -> throw exception;
+      };
+      case UNDER_COMPLETION -> switch (next) {
+        case PENDING, TO_CORRECT, COMPLETED -> next;
+        case UNDER_COMPLETION -> throw exception;
+      };
+      case TO_CORRECT -> switch (next) {
+        case PENDING, UNDER_COMPLETION -> throw exception;
+        case TO_CORRECT, COMPLETED -> next;
+      };
+      case COMPLETED -> switch (next) {
+        case TO_CORRECT, COMPLETED -> next;
+        case PENDING, UNDER_COMPLETION -> throw exception;
+      };
+    };
   }
 }
