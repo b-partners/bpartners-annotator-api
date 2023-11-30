4101bded5dcd68bbaa49a4ad47addba9309b8334
Mahefaa <hei.mahefa@gmail.com>
2023-12-01 01:07:29 +0300

    feat: let user see annotation batches
    refactor: format security conf code for annotation batch reviews

--- src/main/java/api/bpartners/annotator/repository/jpa/AnnotationBatchRepository.java
+++ src/main/java/api/bpartners/annotator/repository/jpa/AnnotationBatchRepository.java
@@ -9,5 +9,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface AnnotationBatchRepository extends JpaRepository<AnnotationBatch, String> {
   List<AnnotationBatch> findAllByTaskId(String taskId, Pageable pageable);
 
+  List<AnnotationBatch> findAllByAnnotatorIdAndTaskId(
+      String annotatorId, String taskId, Pageable pageable);
+
   Optional<AnnotationBatch> findByTaskIdAndId(String taskId, String id);
+
+  Optional<AnnotationBatch> findByAnnotatorIdAndTaskIdAndId(
+      String annotatorId, String taskId, String id);
 }
