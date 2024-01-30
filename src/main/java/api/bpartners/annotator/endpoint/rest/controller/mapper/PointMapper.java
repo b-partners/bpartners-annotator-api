fe3a43f7bb821bd276c7a2a770cd2d8719803a15
Mahefaa <hei.mahefa@gmail.com>
2024-01-30 12:00:33 +0300

    fix: Annotation.Polygon.Point must not be null

--- src/main/java/api/bpartners/annotator/endpoint/rest/controller/mapper/PointMapper.java
+++ src/main/java/api/bpartners/annotator/endpoint/rest/controller/mapper/PointMapper.java
@@ -7,16 +7,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class PointMapper {
   public Point toRest(Annotation.Point domain) {
-    if (domain == null) {
-      return null;
-    }
     return new Point().x(domain.getX()).y(domain.getY());
   }
 
   public Annotation.Point toDomain(Point rest) {
-    if (rest == null) {
-      return null;
-    }
     return Annotation.Point.builder().x(rest.getX()).y(rest.getY()).build();
   }
 }
