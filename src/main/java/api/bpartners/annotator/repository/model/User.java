1ccde4ff7cf0acf7c34f1ccd8049dcaad05a2da6
onitsiky <hei.onitsiky@gmail.com>
2024-02-20 15:46:40 +0300

    fix: user role is not enum but array

--- src/main/java/api/bpartners/annotator/repository/model/User.java
+++ src/main/java/api/bpartners/annotator/repository/model/User.java
@@ -1,6 +1,7 @@
 package api.bpartners.annotator.repository.model;
 
 import static jakarta.persistence.EnumType.STRING;
+import static org.hibernate.type.SqlTypes.ARRAY;
 import static org.hibernate.type.SqlTypes.NAMED_ENUM;
 
 import api.bpartners.annotator.endpoint.rest.security.model.Role;
@@ -30,9 +31,8 @@ public class User {
   @JoinColumn(name = "team_id")
   private Team team;
 
-  @Column(name = "roles", columnDefinition = "user_role[]")
   @Enumerated(STRING)
-  @JdbcTypeCode(NAMED_ENUM)
+  @JdbcTypeCode(ARRAY)
   private Role[] roles;
 
   private String email;
