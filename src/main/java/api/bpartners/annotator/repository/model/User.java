ecc4987320540c0725122c4d80b6c9ed46628566
Mahefaa <hei.mahefa@gmail.com>
2024-03-01 09:07:25 +0300

    minor: format code

--- src/main/java/api/bpartners/annotator/repository/model/User.java
+++ src/main/java/api/bpartners/annotator/repository/model/User.java
@@ -1,15 +1,11 @@
 package api.bpartners.annotator.repository.model;
 
 import static io.hypersistence.utils.hibernate.type.array.internal.AbstractArrayType.SQL_ARRAY_TYPE;
-import static jakarta.persistence.EnumType.STRING;
-import static org.hibernate.type.SqlTypes.ARRAY;
 
 import api.bpartners.annotator.endpoint.rest.security.model.Role;
 import io.hypersistence.utils.hibernate.type.array.EnumArrayType;
-import io.hypersistence.utils.hibernate.type.array.internal.AbstractArrayType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -18,7 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Type;
 
@@ -36,16 +31,9 @@ public class User {
   private Team team;
 
   @Type(
-    value = EnumArrayType.class,
-    parameters = @Parameter(
-      name = SQL_ARRAY_TYPE,
-      value = "user_role"
-    )
-  )
-  @Column(
-    name = "roles",
-    columnDefinition = "user_role[]"
-  )
+      value = EnumArrayType.class,
+      parameters = @Parameter(name = SQL_ARRAY_TYPE, value = "user_role"))
+  @Column(name = "roles", columnDefinition = "user_role[]")
   private Role[] roles;
 
   private String email;
