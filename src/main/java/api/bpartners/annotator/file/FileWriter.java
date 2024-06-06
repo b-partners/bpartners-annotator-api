f2e12f1372496ad12a22181cce2523729f3ab719
Mahefaa <hei.mahefa@gmail.com>
2024-06-06 15:06:40 +0300

    chore: FileWriter has no way to throw IOException, handle with @SneakyThrows

--- src/main/java/api/bpartners/annotator/file/FileWriter.java
+++ src/main/java/api/bpartners/annotator/file/FileWriter.java
@@ -1,15 +1,13 @@
 package api.bpartners.annotator.file;
 
-import static api.bpartners.annotator.model.exception.ApiException.ExceptionType.SERVER_EXCEPTION;
 import static java.util.UUID.randomUUID;
 
-import api.bpartners.annotator.model.exception.ApiException;
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.util.function.BiFunction;
 import javax.annotation.Nullable;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,25 +16,19 @@ public class FileWriter implements BiFunction<byte[], File, File> {
   private final ExtensionGuesser extensionGuesser;
 
   @Override
+  @SneakyThrows
   public File apply(byte[] bytes, @Nullable File directory) {
-    try {
-      String name = randomUUID().toString();
-      String suffix = "." + extensionGuesser.apply(bytes);
-      File tempFile = File.createTempFile(name, suffix, directory);
-      return Files.write(tempFile.toPath(), bytes).toFile();
-    } catch (IOException e) {
-      throw new ApiException(SERVER_EXCEPTION, e);
-    }
+    String name = randomUUID().toString();
+    String suffix = "." + extensionGuesser.apply(bytes);
+    File tempFile = File.createTempFile(name, suffix, directory);
+    return Files.write(tempFile.toPath(), bytes).toFile();
   }
 
+  @SneakyThrows
   public File write(byte[] bytes, @Nullable File directory, String filename) {
-    try {
-      String suffix = extensionGuesser.apply(bytes);
-      File newFile = new File(directory, filename + suffix);
-      Files.createDirectories(newFile.toPath().getParent());
-      return Files.write(newFile.toPath(), bytes).toFile();
-    } catch (IOException e) {
-      throw new ApiException(SERVER_EXCEPTION, e);
-    }
+    String suffix = extensionGuesser.apply(bytes);
+    File newFile = new File(directory, filename + suffix);
+    Files.createDirectories(newFile.toPath().getParent());
+    return Files.write(newFile.toPath(), bytes).toFile();
   }
 }
