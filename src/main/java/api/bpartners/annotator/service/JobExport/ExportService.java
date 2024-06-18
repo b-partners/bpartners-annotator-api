ca1f9bdc9d322577f80f045a367a6226777dd98a
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-06-18 15:23:47 +0300

    chore: split batch then parallelize subbatches export

--- src/main/java/api/bpartners/annotator/service/JobExport/ExportService.java
+++ src/main/java/api/bpartners/annotator/service/JobExport/ExportService.java
@@ -1,18 +1,19 @@
 package api.bpartners.annotator.service.JobExport;
 
-import static api.bpartners.annotator.endpoint.rest.model.ExportFormat.COCO;
-import static api.bpartners.annotator.endpoint.rest.model.ExportFormat.VGG;
 import static org.springframework.transaction.annotation.Propagation.REQUIRED;
 
 import api.bpartners.annotator.endpoint.event.EventProducer;
 import api.bpartners.annotator.endpoint.event.model.JobExportInitiated;
 import api.bpartners.annotator.endpoint.rest.model.ExportFormat;
-import api.bpartners.annotator.model.exception.BadRequestException;
+import api.bpartners.annotator.repository.model.AnnotationBatch;
 import api.bpartners.annotator.repository.model.Job;
 import api.bpartners.annotator.service.AnnotationBatchService;
 import api.bpartners.annotator.service.JobService;
 import jakarta.mail.internet.InternetAddress;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
@@ -35,14 +36,22 @@ public class ExportService {
 
   @Transactional(propagation = REQUIRED, readOnly = true, rollbackFor = Exception.class)
   public Object exportJob(Job job, ExportFormat format) {
-    if (VGG.equals(format)) {
-      var batches = annotationBatchService.findLatestPerTaskByJobId(job.getId());
-      return vggExportService.export(job, batches);
-    }
-    if (COCO.equals(format)) {
-      var batches = annotationBatchService.findLatestPerTaskByJobId(job.getId());
-      return cocoExportService.export(job, batches);
-    }
-    throw new BadRequestException("unknown export format " + format);
+    var batches = annotationBatchService.findLatestPerTaskByJobId(job.getId());
+    var subBatches = partition(batches);
+    return switch (format) {
+      case VGG -> subBatches.parallelStream()
+          .map(batch -> vggExportService.export(job, batch))
+          .toList();
+      case COCO -> subBatches.parallelStream()
+          .map(batch -> cocoExportService.export(job, batch))
+          .toList();
+    };
+  }
+
+  private List<List<AnnotationBatch>> partition(List<AnnotationBatch> batches) {
+    int middleIndex = batches.size() / 2;
+    Map<Boolean, List<AnnotationBatch>> groups =
+        batches.stream().collect(Collectors.partitioningBy(s -> batches.indexOf(s) >= middleIndex));
+    return new ArrayList<>(groups.values());
   }
 }
