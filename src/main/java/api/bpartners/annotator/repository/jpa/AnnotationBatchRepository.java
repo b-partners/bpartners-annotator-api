c7de457d333108195da2d7758e939e8e5c57a0f0
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2024-07-02 14:06:28 +0300

    Revert "Revert "chore(to-revert): export unique batches from annotation batch""
    This reverts commit 7f7566ed3da146807461ff082b99c1be032b944b.

--- src/main/java/api/bpartners/annotator/repository/jpa/AnnotationBatchRepository.java
+++ src/main/java/api/bpartners/annotator/repository/jpa/AnnotationBatchRepository.java
@@ -24,18 +24,12 @@ public interface AnnotationBatchRepository extends JpaRepository<AnnotationBatch
       nativeQuery = true,
       value =
           """
-						    select distinct on (t.id) a.* from annotation_batch a
-						    inner join task t on t.id = a.task_id
-						    inner join job j on j.id = t.job_id
-						    inner join (
-						        select a1.task_id, max(a1.creation_timestamp) as latest_creation_timestamp
-						        from annotation_batch a1
-						    inner join task t1 on t1.id = a1.task_id
-						    inner join job j1 on j1.id = t1.job_id
-						    where j1.id = :jobId
-						    group by a1.task_id
-						    ) max_ct on max_ct.latest_creation_timestamp = a.creation_timestamp
-						    where j.id = :jobId
-						""")
+					 SELECT distinct on (t.id) ab.id, ab.task_id, ab.annotator_id, ab.creation_timestamp, ab.export_task_id FROM annotation_batch_review abr
+					 inner join public.annotation_batch ab on abr.annotation_batch_id = ab.id
+					 inner join public.task t on ab.task_id = t.id
+					 inner join public.job j on j.id = t.job_id
+					 where abr.status = cast('ACCEPTED' as review_status) and j.id = :jobId
+					 group by t.id, ab.id, ab.task_id, ab.annotator_id, ab.creation_timestamp, ab.export_task_id;
+				""")
   List<AnnotationBatch> findLatestPerTaskByJobId(@Param("jobId") String jobId);
 }
